ef9f136f12f82871a3de3bd390ce7a215fd1ed7a
lhuangufl <lhuangufl@gmail.com>
2022-04-18 09:35:59 -0400

    New API: UpdateUserProfile & UpdateVendor

--- backend/packages/db/queries.go
+++ backend/packages/db/queries.go
@@ -14,6 +14,8 @@ const (
 	GetAllRestaurantsQueryByZipCode = `SELECT owneremail, name, phone, zipcode FROM restaurants WHERE zipcode = $1`
 	AddMenuItem                     = `INSERT INTO menuitem(ownerid, price, itemname) VALUES ($1, $2, $3)`
 	GetMenuByOwnerId                = `SELECT m.itermid, r.restid, m.itemname, m.price, m.created_on, m.updated_at FROM restaurants as r, menuitem as m WHERE r.restid = m.ownerid and r.owneremail = $1`
+	UpdateUserProfile               = `UPDATE users SET name = $1, email = $2 WHERE id = $3`
+	UpdateVendorProfile             = `UPDATE restaurants SET owneremail = $1, name = $2, zipcode = $3, phone = $4 WHERE restid = $5`
 
 	CreateOrder = `INSERT INTO gatororder(orderid, ownerid, totalprice) VALUES ($1, $2, $3)`
 	UpdateOrder = `UPDATE gatororder SET totalprice = $1 WHERE orderid = $2 AND ownerid = $3`
